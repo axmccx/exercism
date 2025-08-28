593c36e5157ed0d00b4468bf05d759bcbab62e28
Alex McCulloch <alexmccx@gmail.com>
2025-08-27 22:07:50 -0400

    first anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,22 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &'a [&'a str]) -> HashSet<&'a str> {
+    let mut anagrams = HashSet::new();
+
+    for &possible_anagram in possible_anagrams {
+        let mut letters_to_check: Vec<char> = possible_anagram.chars().collect();
+
+        for char_in_word in word.chars() {
+            if letters_to_check.contains(&char_in_word) {
+                if let Some(c) = letters_to_check.iter().position(|&v| v == char_in_word) {
+                    letters_to_check.remove(c);
+                }
+            }
+        }
+
+        if letters_to_check.is_empty() {
+            anagrams.insert(possible_anagram);
+        }
+    }
+    anagrams
 }
